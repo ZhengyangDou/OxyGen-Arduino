ff68472a45e20bdbc54c978f945f1d6b3c4fd028
Zhengyang <3314076990@qq.com>
2024-04-15 20:31:04 +0800

    新增错误处理

--- main.py
+++ main.py
@@ -5,25 +5,22 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 data = ""
-# 打开串口连接
 ser = serial.Serial('COM3', 9600)
-
-# 从串口读取数据
 def read_data():
     while True:
         global data
-        data = ser.readline().decode().strip()
-
+        try:
+            data = ser.readline().decode().strip()
+        except:
+            continue
 def backend():
     app.run()
 @app.route('/')
 def index():
     return "浴室氧气检测实时数据"
-
 @app.route('/data')
 def get_data():
     return data
-
 if __name__ == '__main__':
     socket_thread = Thread(target=backend)
     socket_thread.start()
